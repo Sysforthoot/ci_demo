6d008673fbfee314648045fe57854e607008be00
Sysforthoot <102728679+Sysforthoot@users.noreply.github.com>
2022-08-08 15:01:22 +0530

    Update ME-user_creation.js

--- cypress/integration/testfolder/ME-user_creation.js
+++ cypress/integration/testfolder/ME-user_creation.js
@@ -7,11 +7,11 @@ describe('cc-app-test 1', () => {
         cy.wait(2000);
         cy.get("#lastName").type("Test_ME");
         cy.wait(2000);
-        cy.get("#emailAddress").type("sysforthoot6@gmail.com");
+        cy.get("#emailAddress").type("sysforthoot15@gmail.com");
         cy.wait(2000);
-        cy.get("#password").type("Sysforth4@2022");
+        cy.get("#password").type("Sysforth15@2022");
         cy.wait(2000);
-        cy.get("#passwordConfirmation").type("Sysforth4@2022");
+        cy.get("#passwordConfirmation").type("Sysforth15@2022");
         cy.wait(2000);
         cy.get('#hospital').type('This is hospital name');
         cy.wait(2000);
@@ -19,9 +19,13 @@ describe('cc-app-test 1', () => {
         cy.wait(2000);
         cy.get("#zipCode").type("12345");
         cy.wait(2000);
-        cy.xpath('/html[1]/body[1]/div[1]/main[1]/div[1]/div[1]/form[1]/div[2]/div[2]/div[2]/label[1]/input[1]').type('123456789');
-        cy.wait(2000);
         
+        
+        cy.xpath('/html[1]/body[1]/div[1]/main[1]/div[1]/div[1]/form[1]/div[2]/div[2]/div[2]/label[1]/input[1]').type('153153153153');
+        cy.wait(2000);
+     
+        
+        cy.get('.m-0 > .bg-white');
         cy.contains('Select...').type("{enter}");
         cy.wait(2000);
         const p = 'testing.pdf';
@@ -32,3 +36,4 @@ describe('cc-app-test 1', () => {
         cy.get('.btn').click();
     })
 })
+
